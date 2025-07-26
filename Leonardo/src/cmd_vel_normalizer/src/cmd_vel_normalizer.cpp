fae032ee2b699904c2e85d1fe7581eb39637e339
EmDonato <mcf.donato@gmail.com>
2025-07-26 15:47:46 +0200

    Removed submodule and added Leonardo ROS2 nodes as regular directory

---
+++ Leonardo/src/cmd_vel_normalizer/src/cmd_vel_normalizer.cpp
@@ -0,0 +1,42 @@
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+
+#define WHEEL_SEPARATION 0.20f
+#define V_MAX 0.362f
+#define W_MAX (2.0f * V_MAX / WHEEL_SEPARATION)
+
+class CmdVelNormalizer : public rclcpp::Node {
+public:
+  CmdVelNormalizer() : Node("cmd_vel_normalizer") {
+    pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel_nav2_norm", 10);
+    sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
+      "cmd_vel_nav2", 10,
+      std::bind(&CmdVelNormalizer::callback, this, std::placeholders::_1));
+  }
+
+private:
+  void callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
+    geometry_msgs::msg::Twist norm_msg;
+
+    norm_msg.linear.x = msg->linear.x / V_MAX;
+    norm_msg.angular.z = msg->angular.z / W_MAX;
+
+    norm_msg.linear.y = 0.0;
+    norm_msg.linear.z = 0.0;
+    norm_msg.angular.x = 0.0;
+    norm_msg.angular.y = 0.0;
+
+    pub_->publish(norm_msg);
+  }
+
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_;
+};
+
+int main(int argc, char * argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CmdVelNormalizer>());
+  rclcpp::shutdown();
+  return 0;
+}
+
